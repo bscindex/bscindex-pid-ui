0e4c05109b7a5a94f3ff9b26c7317840d141ce37
JK Reddy <imjaireddy@gmail.com>
2021-03-01 11:45:54 +0530

    update token info link

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -13,12 +13,12 @@ const config: MenuEntry[] = [
       {
         label: 'Exchange',
         href:
-          'https://cheeseswap.app/#/swap?inputCurrency=0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c&outputCurrency=0xcfdf8a80fecaeecc144fa74c0df8691bfd0e26e3',
+          'https://cheeseswap.app/#/swap?inputCurrency=0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c&outputCurrency=0xc8c7ece03492cc1d99b1cc2bb588254b15de45a5',
       },
       {
         label: 'Liquidity',
         href:
-          'https://cheeseswap.app/#/add/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c/0xcfdf8a80fecaeecc144fa74c0df8691bfd0e26e3',
+          'https://cheeseswap.app/#/add/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c/0xc8c7ece03492cc1d99b1cc2bb588254b15de45a5',
       },
     ],
   },
@@ -42,11 +42,11 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Tokens',
-        href: 'https://info.cheeseswap.app/token/0xcfdf8a80fecaeecc144fa74c0df8691bfd0e26e3',
+        href: 'https://info.cheeseswap.app/token/0xc8c7ece03492cc1d99b1cc2bb588254b15de45a5',
       },
       {
         label: 'Pairs',
-        href: 'https://info.cheeseswap.app/pair/0xdf8481f4ffb688d940f59f839c2613cc6c40a79e',
+        href: 'https://info.cheeseswap.app/pair/0x96bae269454f7d9c0a5e032290aba3f36355579b',
       },
       {
         label: 'Accounts',
